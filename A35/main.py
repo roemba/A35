891a02c630eccf4ca1d3bd399828609cdee267db
Roemer Hendrikx <roemba@gmail.com>
2018-02-19 16:11:39 +0100

    First steps of beam theory

--- A35/main.py
+++ A35/main.py
@@ -1,4 +1,6 @@
 
 from parameters import parameters
+from beamTheory.main import beamTheory
 
-print parameters.height
+beam = beamTheory
+beam.staticEquilibrium(parameters.chord, parameters.span, parameters.xlocation1, parameters.xlocation2, parameters.xlocation3, parameters.d12, parameters.actuatorload, parameters.aerodynamicload)
