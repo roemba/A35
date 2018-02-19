82e584fc82c334fff940d2904b1305730130f438
Roemer Hendrikx <roemba@gmail.com>
2018-02-19 18:20:09 +0100

    Added main.py fixes and started beam theory

--- A35/beamTheory/main.py
+++ A35/beamTheory/main.py
@@ -2,5 +2,6 @@ import numpy as np
 
 class beamTheory:
     def staticEquilibrium(self, c_a, l_a, x_1, x_2, x_3, x_a, P, q):
-        variableMatrix = np.matrix([[1,2]
+        print "test"
+        variableMatrix = np.matrix([[1, 0, 0, 1, 0, 0, 1, 0, ]
                                     ])
--- A35/main.py
+++ A35/main.py
@@ -2,5 +2,5 @@
 from parameters import parameters
 from beamTheory.main import beamTheory
 
-beam = beamTheory
+beam = beamTheory()
 beam.staticEquilibrium(parameters.chord, parameters.span, parameters.xlocation1, parameters.xlocation2, parameters.xlocation3, parameters.d12, parameters.actuatorload, parameters.aerodynamicload)
