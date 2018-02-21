df5abf4911ba271fa7f080d7fd8e13dc8a3e96a1
Panagiotis Papadopoulos <panopapas@gmail.com>
2018-02-21 12:05:56 +0100

    AreaMomentOfInertia beginning code

--- A35/CSGeo/areaMomentOfInertia.py
+++ A35/CSGeo/areaMomentOfInertia.py
@@ -1,3 +1,5 @@
+import numpy as np
+import CSGeometry.CSGeometry as g
 
 """
 
@@ -5,3 +7,31 @@ Area Moment Of Inertia (output in m^4)
 call
 
 """
+
+class areaMomentOfInertia:
+
+    def genericStringer(self, w_st, h_st, t_st):
+
+        a, cy = g.stringer(w_st, h_st, t_st)
+
+        I_zz_b = (((t_st)**3)*w_st)/12. + (((h_st-t_st)**3)*t_st)/12.
+        I_yy_b = (((t_st**3.)*(h_st-t_st))/12.) + (((w_st**3)*t_st)/12.)
+
+        #Steiner Term
+        I_zz_s = (((cy-(t_st/2.))**2)*t_st*w_st) + ((((h_st-t_st)/2.)-cy)**2)*(h_st-t_st)*t_st
+        I_yy_s = 0.
+
+        I_zz = I_zz_b + I_zz_s
+        I_yy = I_yy_b + I_yy_s
+
+        return I_zz, I_yy, cy
+
+    def rotationEffects(self, I_zz, I_yy, angle):
+
+
+    def getAMOI(self):
+
+
+
+
+        return I_zz, I_yy
