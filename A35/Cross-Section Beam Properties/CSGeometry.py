7993a2ab55c4d3142f279fa88296bcf6888c5490
MvdH~L01 <mark.van.den.ham@gmail.com>
2018-02-21 09:27:49 +0100

    Changed location for some functions from centroid to csgeo. Took into account assumptions for stringer positioning (half spacing offset from TE) from analytical sol. Changed hardcoded numbers to floats

--- A35/Cross-Section Beam Properties/CSGeometry.py
+++ A35/Cross-Section Beam Properties/CSGeometry.py
@@ -12,7 +12,7 @@ n_st
 
 import numpy as np
 
-class stringerPositions:
+class Geometry:
     """
 
     Take h, C_a, n_st to compute stringer coordinates
@@ -21,18 +21,18 @@ class stringerPositions:
 
     """
 
-    def Pos(self, h, C_a, n_st):
-        radius = h/2
+    def stringerPos(self, h, C_a, n_st):
+        radius = h/2.
         triangleLongSection = C_a - radius
         semiCircle = radius * np.pi
         hypo = np.sqrt(radius^2 + triangleLongSection^2)        # Compute length straight part of aileron
-        angle = np.atan(radius / triangleLongSection)           # Compute half-angle
-        length = semiCircle + 2*hypo                            # Total length cross-section
+        angle = np.arctan(radius / triangleLongSection)         # Compute half-angle
+        length = semiCircle + 2.*hypo                           # Total length cross-section
 
         centerSpacing = length / n_st                           # stringer pitch
         finalArray = np.zeros((n_st, 4))
         for i in n_st:
-            distFromTE = centerSpacing * i
+            distFromTE = centerSpacing * (i + 0.5)
 
             if distFromTE > hypo:
                 distFromTE -= hypo
@@ -47,7 +47,7 @@ class stringerPositions:
                     tempAngle = distFromTE / radius
                     y_loc = 0. - np.cos(tempAngle)
                     z_loc = radius - np.sin(tempAngle)
-                    outAngle = -1 * tempAngle           # negative for simplified centroid calc
+                    outAngle = -1 * tempAngle           # negative for simplified centroid & AMOI calc
 
             else:       # distFromTE < hypo
                 y_loc = 0. - distFromTE * np.sin(angle)
@@ -55,6 +55,30 @@ class stringerPositions:
                 outAngle = angle
 
             finalArray[i] = [i, z_loc, y_loc, outAngle]
+        return finalArray
 
+    def stringer(self, w_st, h_st, t_st):
+        stArea = (w_st + h_st) * t_st
+        offsetFromSkin = (h_st * h_st + w_st * t_st) * t_st / (2. * stArea)
+        return stArea, offsetFromSkin
 
+    def straightSkin(self, C_a, h, t_sk):
+        radius = h/2.
+        long = C_a - radius
+        hypo = np.sqrt(long^2 + radius^2)
+        straightArea = hypo * t_sk
+        #dist_z = radius/2           # distance to z axis from centroid ----- due to symmetry irrelevant
+        dist_y = long/2. + radius    # distance to y axis from centroid
+        return straightArea, dist_y
+
+    def circleSkin(self, h, t_sk):
+        radius = h/2.
+        circArea = np.pi * radius * t_sk
+        dist_y = radius - (2. * radius / np.pi)   # check this?
+        return circArea, dist_y
+
+    def spar(self, h, t_sp):
+        spArea = h * t_sp
+        dist_y = h/2.
+        return spArea, dist_y
 
