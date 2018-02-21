67b1886076846299b159fecadcfff99ba803492d
Roemer Hendrikx <roemba@gmail.com>
2018-02-21 10:46:49 +0100

    Added bending and shear analysis of beam

--- A35/beamTheory/main.py
+++ A35/beamTheory/main.py
@@ -18,9 +18,9 @@ class beamTheory:
                                   [-P*(x_a/2.)],
                                   [0.],
                                   [P*(h/2.)-q*l_a*0.25*c_a],
-                                  [(q/24.)*x_1**4 - E*I*d_1],
-                                  [(q/24.)*x_2**4],
-                                  [(q/24.)*x_3**4 - E*I*d_3]
+                                  [(q/24.)*x_1**4. - E*I*d_1],
+                                  [(q/24.)*x_2**4.],
+                                  [(q/24.)*x_3**4. - E*I*d_3]
                                   ])
 
         solutionMatrix = np.linalg.solve(coefficientMatrix, resultMatrix)
@@ -33,3 +33,36 @@ class beamTheory:
             k_1=round(solutionArray[6, :][0], 2), k_2=round(solutionArray[7, :][0], 2))
 
         return solutionMatrix
+
+    def bendingMomentAndShearForcesForX(self, A_y, A_z, B_y, B_z, C_y, R_z, x_3, x_2, x_1, x_a, q, P, x):
+
+        m_zz = q*(1./2.)*x**2.
+        v_y = q*x
+        m_yy = 0.
+        v_z = 0.
+
+        if x >= x_1:
+            m_zz -= A_y*(x-x_1)
+            v_y -= A_y
+            m_yy -= A_z*(x-x_1)
+            v_z -= A_z
+
+        if x >= (x_2 - (x_a/2.)):
+            m_yy -= R_z*(x-x_2+(x_a/2.))
+            v_z -= R_z
+
+        if x >= x_2:
+            m_zz -= B_y*(x-x_2)
+            v_y -= B_y
+            m_yy -= B_z*(x-x_2)
+            v_z -= B_z
+
+        if x >= (x_2 + (x_a/2.)):
+            m_yy += P*(x-x_2-(x_a/2.))
+            v_z += P
+
+        if x >= x_3:
+            m_zz -= C_y*(x-x_3)
+            v_y -= C_y
+
+        return [m_yy, m_zz, v_y, v_z]
--- A35/main.py
+++ A35/main.py
@@ -1,10 +1,27 @@
-
 from parameters import parameters
 from beamTheory.main import beamTheory
+import matplotlib as mplt
+import matplotlib.pyplot as plt
 from CSGeo import *
 import numpy as np
 
 beam = beamTheory()
-beam.staticEquilibrium(parameters.youngsmodulus, 0.000014925648, parameters.chord, parameters.span, parameters.xlocation1, parameters.xlocation2,
-                       parameters.xlocation3, parameters.d12, parameters.actuatorload, parameters.aerodynamicload,
-                       parameters.height, parameters.verticaldisplacementhinge3, parameters.verticaldisplacementhinge1)
+reactionForces = beam.staticEquilibrium(parameters.youngsmodulus, 0.000014925648, parameters.chord, parameters.span,
+                                        parameters.xlocation1, parameters.xlocation2,
+                                        parameters.xlocation3, parameters.d12, parameters.actuatorload,
+                                        parameters.aerodynamicload,
+                                        parameters.height, parameters.verticaldisplacementhinge3,
+                                        parameters.verticaldisplacementhinge1).A1
+
+ytab = []
+xtab = np.linspace(0., parameters.span, num=500)
+for x in xtab:
+    internalForcesArray = beam.bendingMomentAndShearForcesForX(reactionForces[0], reactionForces[1], reactionForces[2],
+                                                               reactionForces[3], reactionForces[4], reactionForces[5],
+                                                               parameters.xlocation3, parameters.xlocation2,
+                                                               parameters.xlocation1, parameters.d12,
+                                                               parameters.aerodynamicload, parameters.actuatorload, x)
+    ytab.append(internalForcesArray[3])
+
+# plt.plot(xtab, ytab)
+# plt.show()
