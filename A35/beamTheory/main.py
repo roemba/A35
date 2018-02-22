a07da7ed6d6b79479ca4c2dceb881f0263b27fa2
Roemer Hendrikx <roemba@gmail.com>
2018-02-22 11:03:07 +0100

    Added new equation for delta c in the z direction

--- A35/beamTheory/main.py
+++ A35/beamTheory/main.py
@@ -2,46 +2,54 @@ import numpy as np
 
 class beamTheory:
     @staticmethod
-    def staticEquilibrium(E, I, c_a, l_a, x_1, x_2, x_3, x_a, P, q, h, d_3, d_1):
-        coefficientMatrix = np.matrix([[1., 0., 1., 0., 1., 0., 0., 0.],
-                                       [0., 1., 0., 1., 0., 1., 0., 0.],
-                                       [0., x_2-x_1, 0., 0., 0., x_a/2., 0., 0.],
-                                       [-(x_2-x_1), 0., 0., 0., x_3-x_2, 0., 0., 0.],
-                                       [0., 0., 0., 0., 0., h/2., 0., 0.],
-                                       [0., 0., 0., 0., 0., 0., x_1, 1.],
-                                       [(1./6.)*(x_2-x_1)**3., 0., 0., 0., 0., 0., x_2, 1.],
-                                       [(1./6.)*(x_3-x_1)**3., 0., (1./6.)*(x_3-x_2)**3., 0., 0., 0., x_3, 1.]
-
+    def staticEquilibrium(E, I_zz, I_yy, c_a, l_a, x_1, x_2, x_3, x_a, P, q, h, d_3, d_1, delta):
+        coefficientMatrix = np.matrix([[1., 0., 1., 0., 1., 0., 0., 0., 0., 0., 0.],
+                                       [0., 1., 0., 1., 0., 1., 0., 0., 0., 0., 0.],
+                                       [0., x_2-x_1, 0., 0., 0., x_a/2., 0., 0., 0., 0., 0.],
+                                       [-(x_2-x_1), 0., 0., 0., x_3-x_2, 0., 0., 0., 0., 0., 0.],
+                                       [0., 0., 0., 0., 0., (h/2.)*np.cos(delta), 0., 0., 0., 0., 0.],
+                                       [0., 0., 0., 0., 0., 0., x_1, 1., 0., 0., 0.],
+                                       [(1./6.)*(x_2-x_1)**3., 0., 0., 0., 0., 0., x_2, 1., 0., 0., 0.],
+                                       [(1./6.)*(x_3-x_1)**3., 0., (1./6.)*(x_3-x_2)**3., 0., 0., 0., x_3, 1., 0., 0., 0.],
+                                       [0., 0., 0., 0., 0., 0., 0., 0., x_1, 1., 0.],
+                                       [0., (1./6.)*(x_2-x_1)**3., 0., 0., 0., (1./6.)*(x_a/2.)**3., 0., 0., x_2, 1., 0.],
+                                       [0., (1./6.)*(x_3-x_1)**3., 0., (1./6.)*(x_3-x_2)**3, 0., (1./6.)*(x_3 - x_2 + (x_a/2.))**3., 0., 0., x_3, 1., E*I_yy],
                                     ])
 
         resultMatrix = np.matrix([[q*l_a],
                                   [P],
                                   [-P*(x_a/2.)],
                                   [0.],
-                                  [P*(h/2.)-q*l_a*0.25*c_a],
-                                  [(q/24.)*x_1**4. - E*I*d_1],
+                                  [P*(h/2.)*np.cos(delta)+q*l_a*(0.25*c_a-(h/2.))*np.cos(delta)],
+                                  [(q/24.)*x_1**4. - E*I_zz*d_1],
                                   [(q/24.)*x_2**4.],
-                                  [(q/24.)*x_3**4. - E*I*d_3]
+                                  [(q/24.)*x_3**4. - E*I_zz*d_3],
+                                  [0],
+                                  [0],
+                                  [P*(1./6.)*(x_3 - x_2 - (x_a/2.))**3]
                                   ])
 
         solutionMatrix = np.linalg.solve(coefficientMatrix, resultMatrix)
 
         solutionArray = np.array(solutionMatrix)
-        print "A_y = {a_y}, A_z = {a_z}, B_y = {b_y}, B_z = {b_z}, C_y = {c_y}, R_z = {r_z}, k_1 = {k_1}, k_2 = {k_2}".format(
+        print "A_y = {a_y}, A_z = {a_z}, B_y = {b_y}, B_z = {b_z}, C_y = {c_y}, R_z = {r_z}, k_1_y = {k_1_y}, k_2_y = {k_2_y}, k_1_z = {k_1_z}, k_2_z = {k_2_z}, delta_c_z = {delta_c_z}".format(
             a_y=round(solutionArray[0, :][0], 2), a_z=round(solutionArray[1, :][0], 2),
             b_y=round(solutionArray[2, :][0], 2), b_z=round(solutionArray[3, :][0], 2),
             c_y=round(solutionArray[4, :][0], 2), r_z=round(solutionArray[5, :][0], 2),
-            k_1=round(solutionArray[6, :][0], 2), k_2=round(solutionArray[7, :][0], 2))
+            k_1_y=round(solutionArray[6, :][0], 2), k_2_y=round(solutionArray[7, :][0], 2),
+            k_1_z=round(solutionArray[8, :][0], 2), k_2_z=round(solutionArray[9, :][0], 2),
+            delta_c_z=solutionArray[10, :][0])
 
         return solutionMatrix
 
     @staticmethod
-    def bendingMomentAndShearForcesForX(A_y, A_z, B_y, B_z, C_y, R_z, x_3, x_2, x_1, x_a, q, P, x):
+    def bendingMomentAndShearForcesForX(A_y, A_z, B_y, B_z, C_y, R_z, x_3, x_2, x_1, x_a, q, P, c_a, h, theta, x):
 
         m_zz = q*(1./2.)*x**2.
         v_y = q*x
         m_yy = 0.
         v_z = 0.
+        T = q*x*(0.25*c_a-(h/2.))*np.cos(theta)
 
         if x >= x_1:
             m_zz -= A_y*(x-x_1)
@@ -52,6 +60,7 @@ class beamTheory:
         if x >= (x_2 - (x_a/2.)):
             m_yy -= R_z*(x-x_2+(x_a/2.))
             v_z -= R_z
+            T -= R_z*(h/2.)*np.cos(theta)
 
         if x >= x_2:
             m_zz -= B_y*(x-x_2)
@@ -62,9 +71,31 @@ class beamTheory:
         if x >= (x_2 + (x_a/2.)):
             m_yy += P*(x-x_2-(x_a/2.))
             v_z += P
+            T += P*(h/2.)
 
         if x >= x_3:
             m_zz -= C_y*(x-x_3)
             v_y -= C_y
 
         return [m_yy, m_zz, v_y, v_z]
+
+    @staticmethod
+    def calculateTorqueForX(q, P, R_z, A_z, A_y, B_z, B_y, C_y, c_a, h, theta, x_3, x_2, x_1, x_a, x_sc, x):
+        T = q*x*(0.25*c_a-x_sc)*np.sin(theta)
+
+        if x >= x_1:
+            T -= (A_z*np.cos(theta) + A_y*np.sin(theta))*((h/2.) - x_sc)
+
+        if x >= (x_2 - (x_a/2.)):
+            T -= R_z*np.cos(theta)*((h/2.) - x_sc) + R_z*np.sin(theta)*(h/2.)
+
+        if x >= x_2:
+            T -= (B_z*np.cos(theta) + B_y*np.sin(theta))*((h/2.) - x_sc)
+
+        if x >= (x_2 + (x_a/2.)):
+            T += P*np.cos(theta)*((h/2.) - x_sc) + P*np.sin(theta)*(h/2.)
+
+        if x >= x_3:
+            T -= C_y*np.sin(theta)*((h/2.) - x_sc)
+
+        return T
--- A35/main.py
+++ A35/main.py
@@ -7,16 +7,17 @@ import matplotlib.pyplot as plt
 from CSGeo import *
 import numpy as np
 
-reactionForces = beamTheory.staticEquilibrium(parameters.youngsmodulus, 0.000014925648, parameters.chord,
+reactionForces = beamTheory.staticEquilibrium(parameters.youngsmodulus, 0.000014925648, 0.000014925648, parameters.chord,
                                               parameters.span,
                                               parameters.xlocation1, parameters.xlocation2,
                                               parameters.xlocation3, parameters.d12, parameters.actuatorload,
                                               parameters.aerodynamicload,
                                               parameters.height, parameters.verticaldisplacementhinge3,
-                                              parameters.verticaldisplacementhinge1).A1
+                                              parameters.verticaldisplacementhinge1, parameters.maxupwarddeflection).A1
 
 ytab = []
 xtab = np.linspace(0., parameters.span, num=500)
+# xtab = [0.0765]
 for x in xtab:
     externalForcesArray = beamTheory.bendingMomentAndShearForcesForX(reactionForces[0], reactionForces[1],
                                                                      reactionForces[2],
@@ -25,21 +26,34 @@ for x in xtab:
                                                                      parameters.xlocation3, parameters.xlocation2,
                                                                      parameters.xlocation1, parameters.d12,
                                                                      parameters.aerodynamicload,
-                                                                     parameters.actuatorload, x)
+                                                                     parameters.actuatorload, parameters.chord,
+                                                                     parameters.height, parameters.maxupwarddeflection,
+                                                                     x)
+
+    torque = beamTheory.calculateTorqueForX(parameters.aerodynamicload, parameters.actuatorload, reactionForces[5],
+                                            reactionForces[1], reactionForces[0], reactionForces[3], reactionForces[2],
+                                            reactionForces[4], parameters.chord, parameters.height,
+                                            parameters.maxupwarddeflection,
+                                            parameters.xlocation3, parameters.xlocation2, parameters.xlocation1,
+                                            parameters.d12,
+                                            parameters.height / 2., x)
 
     m_zz_cs, m_yy_cs = coordinateSwap.APtoCS(externalForcesArray[1], externalForcesArray[0],
                                              parameters.maxupwarddeflection)
     v_z_cs, v_y_cs = coordinateSwap.APtoCS(externalForcesArray[3], externalForcesArray[2],
                                            parameters.maxupwarddeflection)
     internalForcesArray = [m_yy_cs, m_zz_cs, v_y_cs, v_z_cs]
-    ytab.append(internalForcesArray[0])
+    ytab.append(torque)
 
-# plt.plot(xtab, ytab)
-# plt.show()
+plt.plot(xtab, ytab)
+plt.show()
 
 # Setting centroid position for simple calling etc.
 # Setting reference coordinate system at LE
 # (z coincides with chord, positive in direction of flight)
 # (y positive upward flight direction)
-centoid_z = - Centroid.computeCentroid(parameters.chord, parameters.height, parameters.skinthickness, parameters.sparthickness, parameters.stiffenerthickness, parameters.stiffenerwidth, parameters.stiffenerheight,parameters.stiffenernumber )
+centoid_z = - Centroid.computeCentroid(parameters.chord, parameters.height, parameters.skinthickness,
+                                       parameters.sparthickness, parameters.stiffenerthickness,
+                                       parameters.stiffenerwidth, parameters.stiffenerheight,
+                                       parameters.stiffenernumber)
 centroid_y = 0.
