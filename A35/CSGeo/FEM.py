adfac63777595456fb0f87d35972174021cfa353
MvdH~L01 <mark.van.den.ham@gmail.com>
2018-02-23 13:54:50 +0100

    Output still off. output format changed to: [z, y, area, k, k_type, l, l_type, m, m_type] (nested lists were acting weird)

--- A35/CSGeo/FEM.py
+++ A35/CSGeo/FEM.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class FEM:
     """
@@ -67,10 +68,10 @@ class FEM:
             boomsPerStringer = (n_booms - 4) / stringerAmount
 
             normPitch = stringerPitch * stringerAmount / (n_booms - 4)
-            startPitch = startDist / ((boomsPerStringer + 3) / 2)
-            endPitch = endDist / ((boomsPerStringer + 3) / 2)
+            startPitch = startDist / (boomsPerStringer + 5)
+            endPitch = endDist / (boomsPerStringer + 5)
 
-        else:
+        else:       # Because sector 4 (spar) doesn't have any stringers, and equal spacing is the current go-to
             normPitch = length / n_booms
             startPitch = 0
             endPitch = 0
@@ -95,8 +96,8 @@ class FEM:
         n_booms, normPitch, startPitch, endPitch, stringerAmount = FEM.generalSector(n_booms, stringerPitch, startDist,
                                                                                      endDist, hypo, minBooms, name)
 
-        print 'sector 1, 3'
-        print n_booms, normPitch, startPitch, endPitch, stringerAmount
+   #     print 'sector 1, 3'
+    #    print n_booms, normPitch, startPitch, endPitch, stringerAmount
         return n_booms, normPitch, startPitch, endPitch, stringerAmount
 
 
@@ -113,15 +114,14 @@ class FEM:
         # As with sector 1, 3: require skin-booms symmetrically about stringers
         # first: get stringers in section...
         startDist = stringerPitch * (stringersIn1 + 1) - hypo
-        stringerAmount = int((semi + startDist) / stringerPitch)
         endDist = startDist     # Due to symmetry
 
         n_booms, normPitch, startPitch, endPitch, stringerAmount = FEM.generalSector(n_booms, stringerPitch, startDist,
                                                                                      endDist, semi + startDist,
                                                                                      minBooms, name)
 
-        print 'sector 2'
-        print n_booms, normPitch, startPitch, endPitch, stringerAmount
+   #     print 'sector 2'
+    #    print n_booms, normPitch, startPitch, endPitch, stringerAmount
         return n_booms, normPitch, startPitch, endPitch, stringerAmount
 
 
@@ -141,8 +141,8 @@ class FEM:
         n_booms, normPitch, startPitch, endPitch, stringerAmount = FEM.generalSector(n_booms, stringerPitch, startDist,
                                                                                      endDist, height, minBooms, name)
 
-        print 'sector 4'
-        print n_booms, normPitch, startPitch, endPitch, stringerAmount
+     #   print 'sector 4'
+      #  print n_booms, normPitch, startPitch, endPitch, stringerAmount
         return n_booms, normPitch, startPitch, endPitch, stringerAmount
 
 
@@ -170,7 +170,6 @@ class FEM:
 
         # Check each sector value to match spacing of stringers as equally as possible
         # Sectors 1, 3:n_booms, normPitch, startPitch, endPitch, stringerAmount
-
         n_sector_1, normPitch_1, startPitch_1, endPitch_1, stringersIn1 = FEM.sector_1(n_sector_1, stringerPitch, hypo)
         n_sector_3, normPitch_3, endPitch_3, startPitch_3, stringersIn3 =\
             n_sector_1, normPitch_1, startPitch_1, endPitch_1, stringersIn1
@@ -187,11 +186,14 @@ class FEM:
         outputArray[0] = n_sector_1, normPitch_1, startPitch_1, endPitch_1, stringersIn1
         outputArray[2] = n_sector_3, normPitch_3, startPitch_3, endPitch_3, stringersIn3
 
+        # Sector 2:
         outputArray[1] = n_sector_2, normPitch_2, startPitch_2, endPitch_2, stringersIn2
 
         # Sector 4:
         outputArray[3] = n_sector_4, normPitch_4, startPitch_4, endPitch_4, stringersIn4
 
+        print ''
+       # print outputArray
         print ''
         return outputArray
 
@@ -219,12 +221,11 @@ class FEM:
         stringerArea = (h_st + w_st) * t_st
 
         # Array creation
-        # np.vstack([A, [z_pos, y_pos, area, [k, l, m]]])
+        # outArray = np.vstack([A, [z_pos, y_pos, area, [k, l, m]]])
         # boom idx 0:
+        m, m_type = None, None
         outArray = np.array(
-            [(-C_a, 0), 0, (1, 2 * n_sector_1 + n_sector_2 + 2, 'N/A')])  # Yes, that's hardcoded. I know.
-        # Deal with it.
-        m = 'N/A'  # Great, most of that value..
+            [-C_a, 0, int(0), int(1), 'skin', int(2 * n_sector_1 + n_sector_2 + 2), 'skin', m, m_type])
 
         # sector looping:
         for sector in range(4):
@@ -234,16 +235,17 @@ class FEM:
                 normBoomNumber = 0  # There is one case where stringers = 0
             else:
                 normBoomNumber = ((booms - 4) / stringers - 1) / 2  # Follows assumption
+                    # normBoomNumber
 
-            interStringerBooms = 2 * normBoomNumber  # needed mental guidance
+            interStringerBooms = 2 * normBoomNumber  # just like the name
             isStringer = False
             stringerTicker = 0  # ticks through each cycle, is used to determine when isStringer is flipped for area
             collPitch = 0.  # reset per sector: collective boom pitch since start
             startBooms = True  # very obvious
             endBooms = False
-            if startPitch == 0: startBooms = False
+            if startPitch == 0.: startBooms = False
 
-            for boom in range(booms):
+            for boom in range(int(booms)):
                 if startBooms:
                     pitch = startPitch
                 elif endBooms:
@@ -254,55 +256,85 @@ class FEM:
                 if isStringer:
                     area = stringerArea
                 else:
-                    area = 0
+                    area = int(0)
 
                 collPitch += pitch
-                if sector == 1:  # TE -> lower spar intersect
+
+                if sector + 1 == 1:  # TE -> lower spar intersect
                     z = collPitch * np.cos(angle) - C_a
                     y = - collPitch * np.sin(angle)
 
-                if sector == 2:  # lower spar intersect -> upper spar intersect
+                if sector + 1 == 2:  # lower spar intersect -> upper spar intersect
                     z = radius * (np.sin(collPitch / radius) - 1.)
                     y = - radius * np.cos(collPitch / radius)
 
-                if sector == 3:  # upper spar intersect -> TE
+                if sector + 1 == 3:  # upper spar intersect -> TE
                     z = - radius - collPitch * np.cos(angle)
                     y = radius - collPitch * np.sin(angle)
 
-                if sector == 4:  # lower spar intersect -> upper spar intersect
+                if sector + 1 == 4:  # lower spar intersect -> upper spar intersect
                     z = - radius
                     y = - radius + collPitch
 
-                # Let's get neighbouring boom indexes
-                k = np.shape(outArray)[0] - 1  # takes amount of rows. THIS is the issue if there is any
-                l = k + 2
-                np.vstack([outArray, [(z, y), area, (k, l, m)]])
+                # Let's get neighbouring boom
+                k = int(np.shape(outArray)[0] - 1)  # takes amount of rows. THIS is the issue if there is any
+                l = int(k + 2)
+
+                if sector + 1 != 4:
+                    k_type = 'skin'
+                    l_type = 'skin'
+
+                else:
+                    k_type = 'spar'
+                    l_type = 'spar'
+
+                if sector + 1 == 3 and boom == range(int(booms)): l = 0
+                outArray = np.vstack([outArray, [z, y, area, k, k_type, l, l_type, m, m_type]])
 
                 # end of second nested loop:
                 stringerTicker += 1
                 if isStringer:
                     isStringer = False
                     stringerTicker = 0
-                if (startBooms or endBooms) == False and stringerTicker == interStringerBooms:
+
+                if (not (startBooms or endBooms)) and stringerTicker == interStringerBooms:
                     isStringer = True
 
                 if boom + 1 == normBoomNumber + 2:
                     startBooms = False
                     isStringer = True  # because the assumption is how it is designed
-                if boom + 2 == booms - (normBoomNumber + 2):
-                    if not sector == 4:  # Let's prevent weird issues shall we?
+                if boom + 1 == booms - (normBoomNumber + 2):
+                    if not (sector == 4):  # Let's prevent weird issues shall we?
                         endBooms = True
 
             # Again hardcoding; but how else do we get those spar caps in there?
-            if sector == 1:
-                np.vstack([outArray, [(-h / 2, -h / 2), 0, (n_sector_1, n_sector_1 + 2, - n_sector_4)]])
-            if sector == 2:
-                np.vstack(
-                    [outArray, [(-h / 2, -h / 2), 0, (n_sector_2 + n_sector_1 + 1, n_sector_2 + n_sector_1 + 3, -1)]])
+            if sector + 1 == 1:
+                outArray = np.vstack([outArray, [-h / 2, -h / 2, int(0), int(n_sector_1), 'skin', int(n_sector_1 + 2), 'skin', int(- n_sector_4), 'spar']])
+            if sector + 1 == 2:
+                outArray = np.vstack(
+                    [outArray, [-h / 2, h / 2, int(0), int(n_sector_2 + n_sector_1 + 1), 'skin', int(n_sector_2 + n_sector_1 + 3), 'skin', int(-1), 'spar']])
 
         return outArray
 
-print FEM.discretization(18, 17, 5, 0.547, 0.225, 17)
-#b = FEM.discretization(77, 52, 5, 0.547, 0.225, 17)
+testArray = FEM.boomPositions(5, 5, 5, 0.547, 0.225, 17, 0.015, 0.02, 0.0012)
+print testArray[12:12+19, 0:2]
+stringerBoom = np.array([-1, 0])
 
+for i in range(int(np.shape(testArray)[0])):
+    if testArray[i, 2] != 0:
+        stringerBoom = np.vstack([stringerBoom, [testArray[i,0], testArray[i, 1]]])
+
+z = testArray[12:12+19, 0]
+zStringer = stringerBoom[:, 0]
+y = testArray[12:12+19, 1]
+yStringer = stringerBoom[:, 1]
+
+#z = testArray[:, 0]
+#y = testArray[:, 1]
+
+plt.hlines(0, -0.6, 0.05)
+plt.vlines(-0.225/2, - 0.225/2 - 0.05, 0.225/2 + 0.05)
+plt.scatter(z, y)
+plt.scatter(zStringer, yStringer, marker='+', c='red')
+plt.show()
 
