c4a4ba9fe0f09e5cc92e5f431ba86059fca6fdae
MvdH~D01 <mark.van.den.ham@gmail.com>
2018-02-28 17:19:44 +0100

    First version of closedSectionShearFlow (no way to iterate for z_sc yet)

--- A35/shearAnalysis/closedSectionShearFlow.py
+++ A35/shearAnalysis/closedSectionShearFlow.py
@@ -7,11 +7,75 @@ class closedSectionShearFlow:
     boomArray has index: [z, y, area, k, k_type, l, l_type, m, m_type, isStringerBool]
     openShearArray has index: [cellNumber, startBoom, endBoom, q_b]
 
-    Outputs: q_b, z_sc
+    Outputs: q_s01, q_s02, d_theta_d_x
     """
     @staticmethod
-    def calculation(I_zz, I_yy, I_zy, V_z, V_y, h, t_sk, t_sp, boomArray):
+    def perpDistShearCenter(z1, y1, z2, y2, z_sc):
+        num = (z1 + (z1 - z2) / 2. - z_sc) * (y1 - y2) / (z1 - z2)
+        num += y1 + (y1 - y2) / 2.
+        den = np.sqrt((y1 - y2)**2 + (z1 - z2)**2)
+        p = num / den
+        l = den
+        return p, l
 
 
+    @staticmethod
+    def calculation(V_y, C_a, h, t_sk, t_sp, z_sc, boomArray, openShearArray):
 
-        return q_b, z_sc
+        # Areas of cells
+        A1 = np.pi * h / 2.
+        A2 = (C_a - h / 2.) * h / 2.
+
+        # loopArray structure:
+        # [ cellNumber, sum(l_i/t_i), sum_spar(l_i/t_i), sum(q_bi*l_i/t_i), sum(p*l_i*q_bi), sum_spar(p*l_i*q_bi) ]
+        loopArray = np.zeros((2, 6))
+
+        # Is global, not specific to one cell, still returned in array (only use cell 2)
+        p_l_q = 0.
+
+        # Loop cells -> loop panels -> check cell match -> get p, find t
+        for cell in range(1, 3):    # cells 1, 2
+            # (re)set summation values
+            l_t = 0.
+            l_t_spar = 0.
+            q_bi = 0.           # is actually q_bi * l_i / t_i
+            p_l_q_spar = 0.     # We only need the one for cell 1; hence recompute
+            for panel in openShearArray:
+                cellNumber, startBoom, endBoom, q_b, panelIndex = panel
+
+                if int(cellNumber) == cell:
+                    # Find boom values for associated booms
+                    boom1 = boomArray[startBoom]
+                    boom2 = boomArray[endBoom]
+
+                    # p: perp. dist. from line of application
+                    # l: length of panel
+                    p, l = closedSectionShearFlow.perpDistShearCenter(boom1[0], boom2[0], boom1[1], boom2[1], z_sc)
+
+                    # Find index of connection and get t based upon string
+                    # First prevent mistakes if boom[:3] could
+                    boomTemp = boom1[3:3+6:2]
+                    if boom1[boomTemp.index(endBoom) + 1] == 'spar':
+                        t = t_sp
+                        l_t_spar += l / t
+                        p_l_q_spar += p * l * q_b
+                    else:
+                        t = t_sk
+
+                    l_t += l / t
+                    q_bi += q_b * l / t
+                    p_l_q += p * l * q_b
+
+            loopArray[cell - 1] = [l_t, l_t_spar, q_bi, p_l_q, p_l_q_spar]
+
+        # 3x3 matrix for finding qs01, qs02, d(theta)/dx
+        matrix = np.array([[loopArray[0, 1],    -loopArray[0, 2],   -A1 ],
+                           [-loopArray[1, 2],   loopArray[1, 1],    -A2 ],
+                           [2 * A1,             2 * A2,             0   ]])
+        ans = np.array([[-loopArray[0, 3]],
+                        [-loopArray[1, 3]],
+                        [-loopArray[1, 4] + loopArray[0, 5] - V_y * z_sc]])
+
+        q_s01, q_s02, d_theta_d_x = np.linalg.solve(matrix, ans)
+        return q_s01, q_s02, d_theta_d_x
+
