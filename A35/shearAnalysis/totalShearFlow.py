7da6616fcd304521b05858bf5e542ab594948527
MvdH~L01 <mark.van.den.ham@gmail.com>
2018-03-01 16:15:14 +0100

    Added deflection in y due to bending

--- A35/shearAnalysis/totalShearFlow.py
+++ A35/shearAnalysis/totalShearFlow.py
@@ -51,3 +51,38 @@ class shearFlowAndDeflection:
 
         return max_q, idxMax, idx2Max
         #   output format: [max_q, min_q, startBoom_max_q, endBoom_max_q, startBoom_min_q, endBoom_min_q]
+
+
+    @staticmethod
+    def bendingDeflectionInY(E, I_zz, theta, aero_q, x_1, x_2, x_3, x_a, A_y, A_z, B_y, B_z, C_y, k1, k2, R_z, P, x):
+        cos = np.cos(theta)
+        sin = np.sin(theta)
+        div = - E * I_zz * cos
+        qContribution = - aero_q * x**4 * cos
+        rotatedA = A_y * cos + A_z * sin
+        rotatedB = B_y * cos + B_z * sin
+        rotatedC = C_y * cos
+        rotatedP = P * sin
+        rotatedR = R_z * sin
+        stepx1 = (x - x_1)**3 * rotatedA / 6.
+        stepx2 = (x - x_2)**3 * rotatedB / 6.
+        stepx3 = (x - x_3)**3 * rotatedC / 6.
+        stepa1 = (x - x_2 + x_a / 2.) * rotatedR / 6.
+        stepa2 = (x - x_2 - x_a / 2.) * rotatedP / 6.
+
+        deflection = k1 * x + k2 + qContribution
+        if x > x_1:
+            deflection += stepx1
+        if x > x_2:
+            deflection += stepx2
+        if x > x_3:
+            deflection += stepx3
+        if x > x_2 - x_a / 2.:
+            deflection += stepa1
+        if x > x_2 + x_a / 2.:
+            deflection += stepa2
+
+        deflection /= div
+        return deflection
+
+
